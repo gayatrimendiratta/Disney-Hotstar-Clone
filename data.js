ffdefe628468fdb8b19bbf4684d6f839cca622bd
Gayatri Mendiratta <90602404+gayatrimendiratta@users.noreply.github.com>
2023-07-07 11:03:06 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Radha-krishan',
+        des: 'राधाकृष्ण एक भारतीय हिंदी-भाषा की पौराणिक टेलीविजन नाटक श्रृंखला है जिसका प्रीमियर 1 अक्टूबर 2018 को स्टार भारत[1][2] पर किया गया था और यह डिजिटल रूप से डिज्नी + हॉटस्टार पर भी उपलब्ध है।',
+        image: 'images/rk.jpg'
+    },
+    {
+        name: 'ASUR 2',
+        des: 'This web series is a Hindi mythological thriller in which we watch the story of a serial killer who commits crimes with impunity and whom the police are trying to nab. You can watch Asur 2',
+        image: 'images/asur2.webp'
+    },
+    {
+        name: 'The Night Manager',
+        des: 'The Night Manager stars Anil Kapoor, Aditya Roy Kapur and Sobhita Dhulipala,[6][7][8] with Tillotama Shome, Ravi Behl, Saswata Chatterjee in supporting roles.',
+        image: 'images/night.cms'
+    },
+    {
+        name: 'Bhaag Milkha Bhaag',
+        des: 'Partition of India in 1947 caused chaos which resulted in mass religious violence in Punjab in British India, killing the parents of Milkha Singh (Farhan Akhtar).',
+        image: 'images/milkha.webp'
+    },
+    {
+        name: 'luca',
+        des: 'A young boy experiences an unforgettable seaside summer on the Italian Riviera filled with gelato, pasta and endless scooter rides.',
+        image: 'images/slider 5.PNG'
+    }
+]
